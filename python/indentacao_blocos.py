1866e0d8431e66ab3e95bd50ca9c38548cb01bd0
pietrodub <pietroti453@gmail.com>
2026-02-23 00:35:54 -0300

    estruturas de repetição (2)

--- python/indentacao_blocos.py
+++ python/indentacao_blocos.py
@@ -12,4 +12,38 @@ def sacar(valor):
     print("Obrigado por ser nosso cliente!")
 
 
-sacar(100)
+sacar(100 )
+
+#elseif
+
+opcao = int(input("Informe: [1] Sacar / [2] Depositar: "))
+
+if opcao == 1:
+    valor = float(input("Informe a quantida de saque:"))
+elif opcao == 2:
+    valor = float(input("Informe o valor do depósito:"))
+else:
+    exit("Opção inválida")
+
+#if dentro de if
+idade = int(input("Idade:"))
+opcao = int(input("Informe: [1] Sacar / [2] Depositar: "))
+
+
+if idade > 18:
+    if opcao == 1:
+        valor = float(input("Informe a quantida de saque:"))
+    elif opcao == 2:
+        valor = float(input("Informe o valor do depósito:"))
+    else:
+        exit("Opção inválida")
+elif idade > 12 and idade < 18:
+    print("Acesso a recursos limitados!")
+else:
+    print("Acesso Negado!")
+
+
+# if ternário
+
+status = "Garantido" if idade >= 18 else "Negado"
+print(f"Status de entrada: {status}")
